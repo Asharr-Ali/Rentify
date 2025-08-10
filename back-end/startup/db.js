8e1358a74f116b7afb4f51cc223b39449e2053d8
Asharr-Ali <asharalihabib2003@gmail.com>
2025-08-10 11:01:50 +0500

    Test Commit

--- back-end/startup/db.js
+++ back-end/startup/db.js
@@ -3,15 +3,12 @@ const winston = require ('winston');
 
 //Connecting to MongoDB
 module.exports = function () {
-    const db = 'mongodb+srv://asharalihabib2003:Zamam12345@rentify.qgtz7gh.mongodb.net/rentify?retryWrites=true&w=majority&appName=Rentify';
+    const db = 'mongodb+srv://asharalihabib2003:Zamam12345@rentify.qgtz7gh.mongodb.net/Rentify?retryWrites=true&w=majority&appName=Rentify';
 
     mongoose.connect(db, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => winston.info(`Connected to MongoDB...`))
-    .catch(err => {
-        winston.error(`Could not connect to MongoDB: ${err.message}`);
-        process.exit(1); // Exit so Render logs show it failed
-    });
+    .catch(err => winston.error(`Could not connect to MongoDB: ${err.message}`));
 };
