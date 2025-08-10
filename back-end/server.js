8c565a2cd46099adf03ef1a122533f7193779f19
Asharr-Ali <asharalihabib2003@gmail.com>
2025-08-10 10:30:06 +0500

    Log Errors During Deployment

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,25 @@
-const winston = require ('winston');
-const express = require ('express');
-const app = express ();
-const cors = require ('cors');
+const winston = require('winston');
 
-app.use (cors());
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err.message, err.stack);
+  process.exit(1);
+});
 
-require ('./startup/logging')();
-require ('./startup/routes')(app);
-require ('./startup/db')();
-require ('./startup/config')();
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
 
 const PORT = process.env.PORT || 4000;
-app.listen ( PORT, () => winston.info (`Listening at Port ${PORT}...`));
+app.listen(PORT, () => winston.info(`Listening at Port ${PORT}...`));
