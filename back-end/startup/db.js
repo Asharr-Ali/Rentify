0324cd9e8d1304550226fa34ecd1e6a69d6474c7
Asharr-Ali <asharalihabib2003@gmail.com>
2025-08-10 10:59:24 +0500

    Have Changed IPs of DB

--- back-end/startup/db.js
+++ back-end/startup/db.js
@@ -3,6 +3,15 @@ const winston = require ('winston');
 
 //Connecting to MongoDB
 module.exports = function () {
-    mongoose.connect ('mongodb+srv://asharalihabib2003:Zamam12345@rentify.qgtz7gh.mongodb.net/?retryWrites=true&w=majority&appName=Rentify')
-    .then (() => winston.info (`Connected to MongoDB...`));
-}
+    const db = 'mongodb+srv://asharalihabib2003:Zamam12345@rentify.qgtz7gh.mongodb.net/rentify?retryWrites=true&w=majority&appName=Rentify';
+
+    mongoose.connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => winston.info(`Connected to MongoDB...`))
+    .catch(err => {
+        winston.error(`Could not connect to MongoDB: ${err.message}`);
+        process.exit(1); // Exit so Render logs show it failed
+    });
+};
